b02c44619aadf399d6278a389d99cba218ae649a
Mariana Perez <marianacruz23@aragon.unam.mx>
2025-11-27 22:10:46 -0600

    conflicto

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: 'https://lonja-backend-2.onrender.com/api',
 });
 
 client.interceptors.request.use(config => {
